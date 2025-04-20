feec7457f003cfabcc2e77a7c7f1fe6d0aab32a7
mohai <mohai.cse@gmail.com>
2025-04-20 13:54:40 +0600

    Selenium Grid

--- src/test/java/testBase/BaseTest.java
+++ src/test/java/testBase/BaseTest.java
@@ -3,16 +3,20 @@ package testBase;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -33,12 +37,55 @@ public class BaseTest{
         p = new Properties();
         p.load(fileReader);
 
-        switch (browser.toLowerCase()){
-            case "chrome": driver = new ChromeDriver(); break;
-            case "firefox": driver = new FirefoxDriver(); break;
-            case "edge": driver = new EdgeDriver(); break;
-            default: System.out.println("Invalid browser.");return;
+        if(p.getProperty("execution_env").equalsIgnoreCase("remote")) {
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+
+            if(os.equalsIgnoreCase("windows")){
+
+                capabilities.setPlatform(Platform.WIN11);
+
+            }
+            else if (os.equalsIgnoreCase("mac")) {
+
+                capabilities.setPlatform(Platform.MAC);
+
+            }
+            else {
+                System.out.println("No Os is found.");
+            }
+
+            if(browser.equalsIgnoreCase("chrome")){
+
+                capabilities.setBrowserName("chrome");
+
+            }
+            else if(browser.equalsIgnoreCase("edge")){
+
+                capabilities.setBrowserName("MicrosoftEdge");
+
+            }
+            else if(browser.equalsIgnoreCase("firefox"))
+            {
+                capabilities.setBrowserName("firefox");
+            }
+            else{
+                System.out.println("No browser is found.");
+            }
+
+            driver = new RemoteWebDriver(new URL("http://192.168.1.101:4444/wd/hub"),capabilities);
+
+
         }
+        else {
+            switch (browser.toLowerCase()){
+                case "chrome": driver = new ChromeDriver(); break;
+                case "firefox": driver = new FirefoxDriver(); break;
+                case "edge": driver = new EdgeDriver(); break;
+                default: System.out.println("Invalid browser.");return;
+            }
+        }
+
 
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
